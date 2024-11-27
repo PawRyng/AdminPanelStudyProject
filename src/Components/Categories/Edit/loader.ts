eb6cb405b8e81b834904c66a849f1e21cc92852d
PawRyng <p_ryng@wp.pl>
2024-11-27 19:37:27 +0100

    Feature: get category on loading

--- src/Components/Categories/Edit/loader.ts
+++ src/Components/Categories/Edit/loader.ts
@@ -1,28 +1,16 @@
-
-import { redirect } from 'react-router-dom';
-import Cookies from 'js-cookie';
-import api from '../../RefreshToken/refreshToken.ts';
+import { redirect } from "react-router-dom";
+import Cookies from "js-cookie";
+import api from "../../RefreshToken/refreshToken.ts";
 
 export const loader = async ({ params }) => {
-  if(!Cookies.get('token')) return redirect('/login');
-    const { id } = params;
+  if (!Cookies.get("token")) return redirect("/login");
+  const { id } = params;
 
   try {
-    // const response = await api.get(`/ProductCategory/product-categories/${id}`,
-    // {
-    //   headers: {
-    //     "Authorization": `Bearer ${token}`
-    //   }
-    // },
-    // {});
-    // console.log(response)
-    // return response.data.data;
-    return {
-      id,
-      name: "test"
-    }
+    const response = await api.get(`/ProductCategory/product-categories/${id}`);
+    return response.data.data;
   } catch (error) {
-    console.error('Error fetching data:', error.status);
-    return redirect('/dashboard/categories')
+    console.error("Error fetching data:", error.status);
+    return redirect("/dashboard/categories");
   }
-}
+};

071a5e860686dc18eb8c233230e33630d1103155
PawRyng <p_ryng@wp.pl>
2024-11-27 19:30:09 +0100

    Feature: add redirect after edit element

--- src/Components/Products/EditProduct/action.ts
+++ src/Components/Products/EditProduct/action.ts
@@ -1,132 +1,140 @@
-import api from '../../RefreshToken/refreshToken.ts';
+import api from "../../RefreshToken/refreshToken.ts";
 import Cookies from "js-cookie";
-import { vatTypeValidation, priceValidation, currencyValidate, notEmptyValidation, convertImageToBase64 } from "../Validation/productValidation.ts";
+import {
+  vatTypeValidation,
+  priceValidation,
+  currencyValidate,
+  notEmptyValidation,
+  convertImageToBase64,
+} from "../Validation/productValidation.ts";
+import { redirect } from "react-router";
 
 interface ActionParams {
-    request: Request;
-    params: {
-        id: string;
-    };
+  request: Request;
+  params: {
+    id: string;
+  };
 }
 
 interface FormDataFields {
-    image: File | null;
-    productCategoryId: FormDataEntryValue | null;
-    name: FormDataEntryValue | null;
-    description: FormDataEntryValue | null;
-    code: FormDataEntryValue | null;
-    currency: FormDataEntryValue | null;
-    grossPrice: FormDataEntryValue | null;
-    nettoPrice: FormDataEntryValue | null;
-    vatType: FormDataEntryValue | null;
-    vatValue: FormDataEntryValue | null;
+  image: File | null;
+  productCategoryId: FormDataEntryValue | null;
+  name: FormDataEntryValue | null;
+  description: FormDataEntryValue | null;
+  code: FormDataEntryValue | null;
+  currency: FormDataEntryValue | null;
+  grossPrice: FormDataEntryValue | null;
+  nettoPrice: FormDataEntryValue | null;
+  vatType: FormDataEntryValue | null;
+  vatValue: FormDataEntryValue | null;
 }
 
-export async function action({ request, params }: ActionParams): Promise<{ type: string; message: string } | null> {
-    const formData = await request.formData();
+export async function action({
+  request,
+  params,
+}: ActionParams): Promise<{ type: string; message: string } | null> {
+  const formData = await request.formData();
 
-    // Zdefiniowanie wszystkich pól formularza
-    const fields: FormDataFields = {
-        image: formData.get('image') as File | null,
-        productCategoryId: formData.get('productCategoryId'),
-        name: formData.get('name'),
-        description: formData.get('description'),
-        code: formData.get('code'),
-        currency: formData.get('currency'),
-        grossPrice: formData.get('gross_price'),
-        nettoPrice: formData.get('netto_price'),
-        vatType: formData.get('vatType'),
-        vatValue: formData.get('vatValue')
+  // Zdefiniowanie wszystkich pól formularza
+  const fields: FormDataFields = {
+    image: formData.get("image") as File | null,
+    productCategoryId: formData.get("productCategoryId"),
+    name: formData.get("name"),
+    description: formData.get("description"),
+    code: formData.get("code"),
+    currency: formData.get("currency"),
+    grossPrice: formData.get("gross_price"),
+    nettoPrice: formData.get("netto_price"),
+    vatType: formData.get("vatType"),
+    vatValue: formData.get("vatValue"),
+  };
+
+  let base64Image: string | undefined;
+  if (fields.image && fields.image instanceof Blob) {
+    base64Image = await convertImageToBase64(fields.image);
+  }
+  // Walidacja danych z formularza
+  if (!notEmptyValidation(fields.name)) {
+    return {
+      type: "name",
+      message: "field_must_not_be_empty",
+    };
+  }
+
+  if (!notEmptyValidation(fields.productCategoryId))
+    return {
+      type: "productCategoryId",
+      message: "field_not_be_empty",
     };
 
-    let base64Image: string | undefined;
-    if (fields.image && fields.image instanceof Blob) {
-        base64Image = await convertImageToBase64(fields.image);
-        console.log(base64Image)
-    }
-    // Walidacja danych z formularza
-    if (!notEmptyValidation(fields.name)) {
-        return {
-            type: 'name',
-            message: 'field_must_not_be_empty'
-        };
-    }
+  if (!notEmptyValidation(fields.code)) {
+    return {
+      type: "code",
+      message: "field_must_not_be_empty",
+    };
+  }
 
-    if(!notEmptyValidation(fields.productCategoryId)) return {
-        type: 'productCategoryId',
-        message: 'field_not_be_empty'
-    }
+  if (!currencyValidate(fields.currency)) {
+    return {
+      type: "currency",
+      message: "choose_accepted_currency",
+    };
+  }
 
-    if (!notEmptyValidation(fields.code)) {
-        return {
-            type: 'code',
-            message: 'field_must_not_be_empty'
-        };
-    }
+  if (!priceValidation(fields.grossPrice)) {
+    return {
+      type: "gross_price",
+      message: "gross_price_must_be_greater_than_0",
+    };
+  }
 
-    if (!currencyValidate(fields.currency)) {
-        return {
-            type: 'currency',
-            message: 'choose_accepted_currency'
-        };
-    }
+  if (!priceValidation(fields.nettoPrice)) {
+    return {
+      type: "netto_price",
+      message: "netto_price_must_be_greater_than_0",
+    };
+  }
 
-    if (!priceValidation(fields.grossPrice)) {
-        return {
-            type: 'gross_price',
-            message: 'gross_price_must_be_greater_than_0'
-        };
-    }
+  if (!vatTypeValidation(fields.vatType)) {
+    return {
+      type: "vatType",
+      message: "VatType_must_have_only_2_chars",
+    };
+  }
 
-    if (!priceValidation(fields.nettoPrice)) {
-        return {
-            type: 'netto_price',
-            message: 'netto_price_must_be_greater_than_0'
-        };
-    }
+  if (!priceValidation(fields.vatValue)) {
+    return {
+      type: "vatValue",
+      message: "vatValue_must_be_greater_than_0",
+    };
+  }
 
-    if (!vatTypeValidation(fields.vatType)) {
-        return {
-            type: 'vatType',
-            message: 'VatType_must_have_only_2_chars'
-        };
-    }
+  try {
+    const { data } = await api.put(
+      `${process.env.REACT_APP_BACK_END_HOST}:${process.env.REACT_APP_BACK_END_PORT}/Product/product`,
+      {
+        id: params.id,
+        name: fields.name,
+        description: fields.description,
+        productCategoryId: fields.productCategoryId,
+        code: fields.code,
+        netPrice: fields.nettoPrice,
+        grossPrice: fields.grossPrice,
+        vatValue: fields.vatValue,
+        vatType: fields.vatType,
+        currency: fields.currency,
+        productPhotoBase64: base64Image,
+      },
+      {
+        headers: {
+          Authorization: `Bearer ${Cookies.get("token")}`,
+        },
+      }
+    );
 
-    if (!priceValidation(fields.vatValue)) {
-        return {
-            type: 'vatValue',
-            message: 'vatValue_must_be_greater_than_0'
-        };
-    }
-
-    try {
-        console.log(base64Image)
-        const { data } = await api.put(
-            `${process.env.REACT_APP_BACK_END_HOST}:${process.env.REACT_APP_BACK_END_PORT}/Product/product`, 
-            {
-                id: params.id,
-                name: fields.name,
-                description: fields.description,
-                productCategoryId: fields.productCategoryId,
-                code: fields.code,
-                netPrice: fields.nettoPrice,
-                grossPrice: fields.grossPrice,
-                vatValue: fields.vatValue,
-                vatType: fields.vatType,
-                currency: fields.currency,
-                productPhotoBase64: base64Image 
-            },
-            {
-                headers: {
-                    'Authorization': `Bearer ${Cookies.get('token')}`
-                }
-            }
-        );
-        console.log(data)
-
-        return null;
-    } catch (error) {
-        console.error(error);
-        return null;
-    }
+    return redirect(`/dashboard/products/${params.id}`);
+  } catch (error) {
+    console.error(error);
+    return null;
+  }
 }
--- src/Components/RefreshToken/refreshToken.ts
+++ src/Components/RefreshToken/refreshToken.ts
@@ -1,5 +1,5 @@
-import axios, { AxiosRequestConfig, AxiosResponse } from 'axios';
-import Cookies from 'js-cookie';
+import axios, { AxiosRequestConfig, AxiosResponse } from "axios";
+import Cookies from "js-cookie";
 
 const api = axios.create({
   baseURL: `${process.env.REACT_APP_BACK_END_HOST}:${process.env.REACT_APP_BACK_END_PORT}`,
@@ -7,76 +7,77 @@ const api = axios.create({
 
 const refreshToken = async (): Promise<string | null> => {
   try {
-    const refreshToken = Cookies.get('refreshToken');
-    const accessToken = Cookies.get('token');
-    
+    const refreshToken = Cookies.get("refreshToken");
+    const accessToken = Cookies.get("token");
+
     if (!refreshToken) throw new Error("No refresh token found");
 
-    const response: AxiosResponse<{ token: string; refreshToken: string; HttpStatus: number }> = await axios.post(
-        `${process.env.REACT_APP_BACK_END_HOST}:${process.env.REACT_APP_BACK_END_PORT}/Authentication/refresh-token`,
-        {
-          refreshToken,
+    const response: AxiosResponse<{
+      token: string;
+      refreshToken: string;
+      HttpStatus: number;
+    }> = await axios.post(
+      `${process.env.REACT_APP_BACK_END_HOST}:${process.env.REACT_APP_BACK_END_PORT}/Authentication/refresh-token`,
+      {
+        refreshToken,
+      },
+      {
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
         },
-        {
-          headers: {
-            'Authorization': `Bearer ${accessToken}`,
-          },
-        }
-      );
-
-      if(response.data.HttpStatus === 0){
-        Cookies.remove('refreshToken');
-        Cookies.remove('token');
-        window.location = "/";
-        return null;
       }
-    Cookies.set('token', 
-        response.data.token,
-         {
-            path: '/',
-            sameSite: 'Lax',
-            // httpOnly: true,
-            secure: process.env.NODE_ENV === 'production',
-         });
-    Cookies.set('refreshToken', 
-        response.data.refreshToken,
-         { 
-            path: '/',
-            sameSite: 'Lax',
-            // httpOnly: true,
-            secure: process.env.NODE_ENV === 'production',
-         });
+    );
 
-  return response.data.token;
-} catch (error) {
-    console.error('Error refreshing token:', error);
-    
-    Cookies.remove('refreshToken');
-    Cookies.remove('token');
+    if (response.data.HttpStatus === 0) {
+      Cookies.remove("refreshToken");
+      Cookies.remove("token");
+      window.location = "/";
+      return null;
+    }
+    Cookies.set("token", response.data.token, {
+      path: "/",
+      sameSite: "Lax",
+      // httpOnly: true,
+      secure: process.env.NODE_ENV === "production",
+    });
+    Cookies.set("refreshToken", response.data.refreshToken, {
+      path: "/",
+      sameSite: "Lax",
+      // httpOnly: true,
+      secure: process.env.NODE_ENV === "production",
+    });
+
+    return response.data.token;
+  } catch (error) {
+    console.error("Error refreshing token:", error);
+
+    Cookies.remove("refreshToken");
+    Cookies.remove("token");
     window.location = "/";
-    return null
+    return null;
   }
 };
 
 api.interceptors.response.use(
   (response: AxiosResponse) => response,
   async (error: any) => {
-    const originalRequest: AxiosRequestConfig & { _retry?: boolean } = error.config;
-    
+    const originalRequest: AxiosRequestConfig & { _retry?: boolean } =
+      error.config;
+
     if (error.response?.status === 401 && !originalRequest._retry) {
       originalRequest._retry = true;
 
       const newAccessToken = await refreshToken();
-      
+
       if (newAccessToken) {
         originalRequest.headers = {
           ...originalRequest.headers,
-          'Authorization': `Bearer ${newAccessToken}`,
+          Authorization: `Bearer ${newAccessToken}`,
         };
         return api(originalRequest);
       }
     }
-console.log(error)
+    console.log(error);
     return Promise.reject(error);
   }
 );
